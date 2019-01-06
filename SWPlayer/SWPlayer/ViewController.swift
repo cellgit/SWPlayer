0ebd774f53dc060ffb66b4d982495d59ca6c8d39
kokora <liuhongli2120@gmail.com>
2019-01-06 13:03:41 +0800

    add project

---
+++ SWPlayer/SWPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWPlayer
+//
+//  Created by 刘宏立 on 2019/1/6.
+//  Copyright © 2019 lhl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SWPlayer/SWPlayerTests/SWPlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SWPlayerTests.swift
+//  SWPlayerTests
+//
+//  Created by 刘宏立 on 2019/1/6.
+//  Copyright © 2019 lhl. All rights reserved.
+//
+
+import XCTest
+@testable import SWPlayer
+
+class SWPlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
